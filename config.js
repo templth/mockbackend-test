6b070a89f0fae279def31a3d14e0b9eed8846e24
Will Munn <willmunn@hotmail.com>
2016-06-14 10:39:07 +0100

    break create connection

--- config.js
+++ config.js
@@ -1,43 +1,47 @@
-System.config({
-  //map tells the System loader where to look for things
-  map: {
-    app: "./src",
-    //'@angular': 'http://localhost:8000/all/@angular',
-    '@angular': 'node_modules/@angular',
-    'rxjs': 'node_modules/rxjs'
-  },
-  //packages defines our app package
-  packages: {
-    app: {
-      main: './main.ts',
-      defaultExtension: 'js'
-    },
-    '@angular/core': {
-      main: 'index.umd.js',
-      defaultExtension: 'js'
-    },
-    '@angular/compiler': {
-      main: 'index.umd.js',
-      defaultExtension: 'js'
-    },
-    '@angular/common': {
-      main: 'index.umd.js',
-      defaultExtension: 'js'
-    },
-    '@angular/http': {
-      main: 'index.umd.js',
-      defaultExtension: 'js'
-    },
-    '@angular/platform-browser-dynamic': {
-      main: 'index.umd.js',
-      defaultExtension: 'js'
-    },
-    '@angular/platform-browser': {
-      main: 'index.umd.js',
-      defaultExtension: 'js'
-    },
-    rxjs: {
-      defaultExtension: 'js'
-    }
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+(function(global) {
+  // map tells the System loader where to look for things
+  var map = {
+    'app':                        'src', // 'dist',
+    '@angular':                   'node_modules/@angular',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    'rxjs':                       'node_modules/rxjs'
+  };
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'rxjs':                       { defaultExtension: 'js', main: 'bundles/Rx.js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }
+  };
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'http',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+    'router-deprecated',
+    'upgrade',
+  ];
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
   }
-});
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+  };
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
+  var config = {
+    map: map,
+    packages: packages
+  }
+  System.config(config);
+})(this);
